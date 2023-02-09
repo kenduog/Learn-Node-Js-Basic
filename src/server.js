297580804cd2a794c66c55a9cd93cc0180b6a28a
kenduog <82100382+kenduog@users.noreply.github.com>
2023-02-09 14:30:30 +0700

    static file in express

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import configViewEngine from './configs/viewEngine'
+require('dotenv').config()
 
 const app = express();
-const port = 8080
+const port = process.env.PORT;
+console.log('>>>>> Check Port:', port);
 
 configViewEngine(app);
 
