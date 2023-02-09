8b9aa2181cf2436b23742a30cb5f10b762e76e1f
kenduog <82100382+kenduog@users.noreply.github.com>
2023-02-09 16:16:23 +0700

    connect db mySQL

---
+++ src/configs/connectDB.js
@@ -0,0 +1,20 @@
+import mysql from 'mysql2'
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'nodejs_basic'
+})
+
+// // simple query
+// connection.query('SELECT * FROM `users`', function (err, results, fields) {
+//   console.log('>>>>>>> Check mySql2')
+//   console.log(results) // results contains rows returned by server
+//   let rows = results.map(row => {
+//     return row
+//   })
+//   console.log(rows) // results contains rows returned by server
+// })
+
+export default connection
--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,6 +1,23 @@
+import connection from '../configs/connectDB'
+
 let getHomePage = (req, res) => {
   //logic
-  return res.render('index.ejs')
+  // simple query
+  let data = []
+  connection.query('SELECT * FROM `users`', function (err, results, fields) {
+    results.map(row => {
+      {
+        data.push({
+          id: row.id,
+          firstName: row.firstName,
+          lastName: row.lastName,
+          email: row.email,
+          address: row.address
+        })
+      }
+    })
+    return res.render('index.ejs', { dataUser: JSON.stringify(data) })
+  })
 }
 let getAboutPage = (req, res) => {
   //logic
