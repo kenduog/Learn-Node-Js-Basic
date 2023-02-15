2b4ef6d0922b5082b222fadb719e115a2eefc853
kenduog <82100382+kenduog@users.noreply.github.com>
2023-02-15 17:06:28 +0700

    update edit delete for project and complete CRUD

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -4,15 +4,14 @@ let getHomePage = async (req, res) => {
   const [rows, fields] = await pool.execute("SELECT * FROM users");
   return res.render("index.ejs", { dataUser: rows });
 };
+
 let getDetailPage = async (req, res) => {
   let userId = req.params.userId;
   let [user] = await pool.execute("select * from users where id = ?", [userId]);
-  console.log(user);
   return res.send(JSON.stringify(user));
 };
 
 let createNewUser = async (req, res) => {
-  console.log("check req: ", req.body);
   let { firstName, lastName, email, address } = req.body; // Destructuring
   await pool.execute(
     "insert into users(firstName, lastName, email, address) values(?, ?, ?, ?)",
@@ -21,8 +20,32 @@ let createNewUser = async (req, res) => {
   return res.redirect("/");
 };
 
+let deleteUser = async (req, res) => {
+  let userId = req.body.userId;
+  await pool.execute("delete from users where id = ?", [userId]);
+  return res.redirect("/");
+};
+
+let getEditPage = async (req, res) => {
+  let userId = req.params.userId;
+  let [user] = await pool.execute("select * from users where id = ?", [userId]);
+  return res.render("edit-user.ejs", { dataUser: user[0] }); // x <- y
+};
+
+let postUpdateUser = async (req, res) => {
+  let { firstName, lastName, email, address, userId } = req.body; // Destructuring
+  await pool.execute(
+    "update users set firstName = ?, lastName = ?, email = ?, address = ? where id = ?",
+    [firstName, lastName, email, address, userId]
+  );
+  return res.redirect("/");
+};
+
 module.exports = {
   getHomePage,
   getDetailPage,
   createNewUser,
+  deleteUser,
+  getEditPage,
+  postUpdateUser,
 };
