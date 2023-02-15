ce452f5ba9b3eabccd600b917e7f9fef74b6c100
kenduog <82100382+kenduog@users.noreply.github.com>
2023-02-15 13:52:57 +0700

    insert value to db - use method POST - router.post

--- src/server.js
+++ src/server.js
@@ -1,19 +1,22 @@
-import express from 'express'
-import configViewEngine from './configs/viewEngine'
-import initWebRoute from './route/web'
+import express from "express";
+import configViewEngine from "./configs/viewEngine";
+import initWebRoute from "./route/web";
 // import connection from './configs/connectDB'
 
-require('dotenv').config()
+require("dotenv").config();
 
-const app = express()
-const port = process.env.PORT
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // setup view engine
-configViewEngine(app)
+configViewEngine(app);
 
 // init web route
-initWebRoute(app)
+initWebRoute(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
