5e32319d0a019ef36b27f4e1a6f579afc4d16895
kenduog <82100382+kenduog@users.noreply.github.com>
2023-02-27 16:10:11 +0700

    midleware and use morgan package check log

--- src/server.js
+++ src/server.js
@@ -5,9 +5,18 @@ import initAPIRoute from "./route/api";
 // import connection from './configs/connectDB'
 
 require("dotenv").config();
+var morgan = require("morgan");
 
 const app = express();
 const port = process.env.PORT;
+
+app.use((req, res, next) => {
+  // check log
+  console.log(req.method);
+  next();
+});
+
+app.use(morgan("combined"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -20,6 +29,11 @@ initWebRoute(app);
 
 //init api route
 initAPIRoute(app);
+
+//handle 404 not found
+app.use((req, res) => {
+  return res.render("not_found.ejs");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
